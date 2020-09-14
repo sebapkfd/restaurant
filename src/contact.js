f88506e98d5cc9ffc770fb91db0f8c8654c41f6b
Seba Soto <sebastian.pkfd@gmail.com>
2020-09-14 20:34:47 -0300

    Basic structure

---
+++ src/contact.js
@@ -0,0 +1,28 @@
+const divContent = document.querySelector('#content');
+
+function renderTitle(){
+    let titleDiv = document.createElement('div');
+    let h1 = document.createElement('h1');
+    h1.textContent = 'Hello';
+
+    titleDiv.appendChild(h1);
+    divContent.appendChild(titleDiv);
+}
+
+function renderInfo(){
+    let infoDiv = document.createElement('div');
+    let h2 = document.createElement('h2');
+    h2.textContent = 'This de Contact Us page'
+
+    infoDiv.appendChild(h2);
+    divContent.appendChild(infoDiv);
+}
+
+
+const renderContact = () =>{
+    renderTitle();
+    renderInfo();
+    return divContent;
+}
+
+export default renderContact
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import renderAbout from './about'
+import renderContact from './contact'
+import renderMenu from './menu'
+
+renderAbout();
+// renderContact();
+// renderMenu();
+
+
+
+
 console.log('xdddd');
